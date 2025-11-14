582c48c83ca528c02d680239c3755d56c385de2e
aleyah-sidhu <aleyah.sidhu@gmail.com>
2025-11-13 19:52:11 -0600

    added inference data

--- VariableReduction.py
+++ VariableReduction.py
@@ -10,6 +10,7 @@ train_data_reduced = pd.read_csv('/Users/aleyahsidhu/vscode projects/Fall-25-Dat
 test_data_encoded = pd.read_csv('/Users/aleyahsidhu/vscode projects/Fall-25-Datathon-/Datasets/test_data_encoded.csv')
 train_data_encoded = pd.read_csv('/Users/aleyahsidhu/vscode projects/Fall-25-Datathon-/Datasets/train_data_encoded.csv')
 
+inference_data_reduced = pd.read_csv('/Users/aleyahsidhu/vscode projects/Fall-25-Datathon-/Datasets/inference_data_reduced.csv')
 
 #print(train_data_encoded.columns)
 
@@ -22,10 +23,10 @@ predictors = ['max_power', 'trm_len_6', 'engine_type_dissel', 'low_education_ind
        'veh_body_RDSTR', 'veh_body_UTE', 'veh_body_CONVT',
        'driving_history_score', 'veh_body_HDTOP', 'veh_color_brown']
 
-xtrain = train_data_encoded[predictors]
+xinference = inference_data_reduced[predictors]
 
 #generate a histogram and heatmap showing the correlation between predictors to guide variable selection
-corr_val_df = round(xtrain.corr(method='kendall'), 2)
+corr_val_df = round(xinference.corr(method='kendall'), 2)
 corr_val_vec = pd.Series( [item for sublist in corr_val_df.values.tolist() for item in sublist if item < 1.0])
 rcParams['figure.figsize'] = 15,4
 corr_val_vec.plot.hist(bins = 50, figsize = (15,5), xlim=(-1,1), grid=True)
